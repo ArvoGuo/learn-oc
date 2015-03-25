7443082c408537e2faef792400b41d7c662fa603
Arvo <ArvoGuo@gmail.com>
2015-03-25 11:25:40 +0800

    Bool

---
+++ BOOL Party/BOOL Party/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BOOL Party
+//
+//  Created by Arvo on 15/3/25.
+//  Copyright (c) 2015年 Arvo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
